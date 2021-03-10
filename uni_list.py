ac0dc8eeb2f598c59306fe451fa658ce98053971
Andrew Black <andrew@black1.org.uk>
2021-03-10 16:41:43 +0000

    print unicode

--- uni_list.py
+++ uni_list.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 import argparse
 import sys
-import re
+import unicodedata
 
+#----------------------------------------------------------
+def print_unicode ( unicode_char ) :
+
+    # print ( "%x %s" % (unicode_int,chr(unicode_int) ) , end=" ")
+    print  (
+       unicode_char,
+       unicodedata.name(unicode_char)
+    )
+
+#----------------------------------------------------------
 # arguments
 filename = sys.argv[1]
 
 with open(filename) as fh :
   for line in fh:
     for (i,c) in enumerate(line) :
-        print (c)
+        print_unicode (c)
