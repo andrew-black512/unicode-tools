4b7e71726ddd267ff2e9e81a9f64453fb31d71e3
Andrew Black <andrew@black1.org.uk>
2024-05-17 12:02:02 +0100

    Add a param

--- redact.py
+++ redact.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python2
+import clipboard
+import re
+
 # Regular expression to match any vowel (case-insensitive)
 vowel_pattern = r"[aeiouAEIOU]"
 
-def substitute_vowels_regex(text):
+def substitute_vowels_regex(text, p_pattern):
   """Substitutes vowels in a string with "*" using regular expression.
 
   Args:
@@ -10,7 +14,11 @@ def substitute_vowels_regex(text):
   Returns:
     The string with vowels replaced by "*".
   """
-  return re.sub(vowel_pattern, "*", text)
+  return re.sub(p_pattern, "*", text)
 
 # Get the clipboard contents
 text = clipboard.paste()
+new_text = substitute_vowels_regex(text,vowel_pattern)
+clipboard.copy( new_text) 
+print( new_text )
+
