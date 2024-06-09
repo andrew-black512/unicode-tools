9b54510e32315bbde0f50f98bfddf6c071e0e430
Andrew Black <andrew@black1.org.uk>
2024-06-09 01:26:45 +0100

    Add no copies

--- incipit.py
+++ incipit.py
@@ -2,7 +2,7 @@
 import sys
 import PyPDF2
 
-def  extract_first_n_pages(input_pdf, w,  num_pages):
+def  extract_first_n_pages(input_pdf, w, num_pages, num_copies ):
       """
       Extracts the first N pages from a PDF and saves them to a new PDF.
 
@@ -12,9 +12,11 @@ def  extract_first_n_pages(input_pdf, w,  num_pages):
       """
       with open(input_pdf, 'rb') as input_file:
         reader = PyPDF2.PdfReader(input_file)
+        #TODO iterate on num_pages
         page = reader.pages[0]
-        print(page.extract_text())
-        w.add_page(page)
+        print(page.extract_text()) #TODO a diff program
+        for n in range(num_copies) :
+           w.add_page(page)
 
 
 filenames = sys.argv[1:]
@@ -22,6 +24,6 @@ writer = PyPDF2.PdfWriter()
 
 for f in filenames :
     print (f)
-    extract_first_n_pages( f, writer, 1 )
+    extract_first_n_pages( f, writer, 1, num_copies=2  )
             
 writer.write("output.pdf")
